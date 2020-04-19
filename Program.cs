db3284eaa482f614d513f8a06cd2714953851b40
Alex Goris <alex.goris@fastlikehell.com>
2020-04-19 14:55:45 +0200

    chore: :tada: Initial project commit
    Currently only blinks 2 LEDs in a threaded a fashion

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System;
+using System.Device.Gpio;
+using System.Collections.Generic;
+
+namespace Pi.NetTest
+{
+   class Program
+   {
+      private static GpioController _io;
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+         Console.WriteLine($"Running in {RuntimeInformation.OSDescription}");
+
+         _io = new GpioController();
+         _io.OpenPin(17, PinMode.Output);
+         _io.OpenPin(18, PinMode.Output);
+
+         var tasks = new List<Task>();
+
+         tasks.Add(Task.Factory.StartNew(() => blinkLed(17)));
+         tasks.Add(Task.Factory.StartNew(() => blinkLed(18)));
+
+         Task.WaitAll(tasks.ToArray());
+
+      }
+
+      static void blinkLed(int pin, int onTime = 1000, int offTime = 200)
+      {
+         while (true)
+         {
+            _io.Write(pin, PinValue.High);
+            Thread.Sleep(onTime);
+            _io.Write(pin, PinValue.Low);
+            Thread.Sleep(offTime);
+         }
+      }
+   }
+}
